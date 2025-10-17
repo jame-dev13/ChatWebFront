1f300864defecba422e1efb15af83de1ad9ad90f
jame-dev13 <jam.dev013@gmail.com>
2025-10-17 11:33:45 -0600

    feat(ChatBox): messages made of type Object and set key actions for buttons and inputs.

--- webChat/src/components/ChatBox.jsx
+++ webChat/src/components/ChatBox.jsx
@@ -3,57 +3,89 @@ import '../styles/chatBox.css';
 import { Message } from './Message';
 
 export const ChatBox = () => {
-  const colorOdd = "#4C3BCF";
-  const colorEven = "#3B9797";
+  const [name, setName] = useState('');
+  const [inputName, setInputName] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [inputMsg, setInputMsg] = useState('');
+  const msgEndRef = useRef(null);
+  const inputMsgRef = useRef(null);
 
-  const [msg, setMsg] = useState('');
-  const [displayMsg, setDisplayMsg] = useState([]);
-  const lastMessage = useRef(null);
-  
-  const handleInputChange = (e) => {
-    setMsg(e.target.value);
-  };
-  
-  const handleSetMsg = () => {
-    if (msg.trim() !== '') {
-      setDisplayMsg([...displayMsg, msg]);
-      setMsg('');
-    }
-  };
+  //utils
+  const enterOrClick = (e) => e.key === 'Enter' || e.type === 'click';
+  const isEmpty = (str) => str.trim() === '';
+  let isNameSet = inputName.trim() !== '';
 
-  /**
-   * Scroll to the bottom of the chat box when a new message is added
-   */
-  const scrollToBottom = () => {
-    if (lastMessage.current) {
-      lastMessage.current.scrollIntoView({ behavior: 'smooth' });
+  //handlers
+  const handleSetNameOnChange = (e) => setName(e.target.value);
+
+  const hanldeOnKeyDown = (e, callback) => {
+    if(e.key === 'Enter'){
+      callback(e);
     }
   }
-  
-  const isEven = (num) => num % 2 === 0;
-  
-  const getBgColor = (index) => {
-    return isEven(index + 1) ? colorEven : colorOdd;
+
+  const handleSetNameOnClick = (e) => {
+    if (isEmpty(name)) return;
+    if (enterOrClick(e)) {
+      setInputName(name);
+      setName('');
+      inputMsgRef.current.focus();
+    }
   }
 
-  /**
-   * Do scroll to bottom when a new message is added
-   */
+  const handleSetMsgOnChange = (e) => setInputMsg(e.target.value);
+
+  const handleSetMsgOnClick = (e) => {
+    if (isEmpty(inputMsg)) return;
+    if(enterOrClick(e)) {
+      const newMsg = {
+        from: inputName,
+        msg: inputMsg,
+        color: "#4C3BCF"
+      }
+      setMessages(prev => [...prev, newMsg]);
+      setInputMsg('');
+      inputMsgRef.current.value = '';
+      inputMsgRef.current.focus();
+    }
+  }
+
+  const scrollToBottom = () => msgEndRef.current?.scrollIntoView({behavior: 'smooth'});
+  
   useEffect(() => {
     scrollToBottom();
-  }, [displayMsg]);
+  }, [messages]);
 
   return (
     <>
-      <div className='container-fluid bg-dark text-white rounded shadow-lg'>
+      <div className='container w-50 h-50 bg-dark text-white rounded shadow-lg'>
+        <div className='row py-2 my-2 justify-content-center align-content-center'>
+          <div className="form-floating mb-3 text-black d-flex">
+            <input type="text"
+              className='form-control mx-2'
+              placeholder="Your name...."
+              name="name"
+              id="name"
+              value={name}
+              onChange={handleSetNameOnChange}
+              onKeyDown={(e) => hanldeOnKeyDown(e, handleSetNameOnClick)}
+              disabled={isNameSet}
+            />
+            <label htmlFor="name" className='mx-4'>Your name</label>
+            <button type='button' className='btn btn-primary'
+            onClick={handleSetNameOnClick}
+            disabled={isNameSet}
+            >Set</button>
+          </div>
+        </div>
         <div className="row">
-          <div className="col d-flex flex-column msgBox text-wrap text-break bg-dark">
-            {
-              displayMsg.map((message, index) => (
-                <Message key={index} from={`user ${index + 1}`} msg={message} bgColor={getBgColor(index)}/>
-              ))
+          <div className="msgBox col d-flex flex-column text-wrap text-break bg-dark">
+            {/* Component Message pending... */}
+            {messages.map((message, index) => (
+              <Message key={index} from={message.from} msg={message.msg} color={inputName === message.from ? "#4C3BCF": "#3B9797"} />
+            ))
             }
-            <div ref={lastMessage} />
+            <div ref={msgEndRef}/>
           </div>
         </div>
         <div className="row">
@@ -62,13 +94,15 @@ export const ChatBox = () => {
               type="text"
               name="msg"
               id="msg"
-              className='form-control mx-2'
+              className='form-control mx-2 p-3'
               placeholder='Message....'
-              value={msg}
-              onChange={handleInputChange}
+              value={inputMsg}
+              onChange={handleSetMsgOnChange}
+              onKeyDown={e => hanldeOnKeyDown(e, handleSetMsgOnClick)}
+              ref={inputMsgRef}
             />
-            <button type='button' className='btn btn-primary' onClick={handleSetMsg}>
-              <i className="fa-solid fa-paper-plane"></i>
+            <button type='button' className='btn btn-primary'>
+              <i className="fa-solid fa-square-arrow-up-right"></i>
             </button>
           </div>
         </div>
--- webChat/src/components/Message.jsx
+++ webChat/src/components/Message.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../styles/message.css';
-export const Message = ({from, msg, bgColor}) => {
+export const Message = ({from, msg, color}) => {
+
   return (
     <>
-    <div className='row box w-50 flex-column rounded m-1 p-2 text-white gap-2' style={{ background: bgColor }}>
+    <div className='row box w-50 flex-column rounded m-1 p-2 text-white gap-2' style={{backgroundColor: color}}>
         <div className="col border-bottom border-info text-start fs-5 fst-italic text-capitalize">{from}:</div>
         <div className="msg col fs-6 fst-italic fw-bold text-start">{msg}</div>
     </div>
